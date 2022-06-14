b085742683592496acb80839607f36393bd5dfa4
sclock <1342810270@qq.com>
2022-06-14 14:51:36 +0800

    修改为消息测试

--- test.py
+++ test.py
@@ -1,10 +1,7 @@
-import datetime
-
 from nonebot.adapters.onebot.v12 import *
-from typing_extensions import Literal
 
-from nonebot_walleq_extension import WQFriendAddRequestEvent as test_event
 from nonebot_walleq_extension import *
+from nonebot_walleq_extension import WQGroupTempMessageEvent as test_event
 
 # 引入测试event
 # Adapter.add_custom_model((test_event),
@@ -14,27 +11,37 @@ from nonebot_walleq_extension import *
 
 # 构建测试数据
 test_data = {
-    'id': '00000000-0000-0000-16f7-e2701484e534',
-    'impl': 'Walle-Q', 'platform': 'qq',
+    'id': '00000000-0000-0000-16f8-6865f4609ed4',
+    'impl': 'Walle-Q',
+    'platform': 'qq',
     'self_id': '123456789',
-    'time': 1655036480.0,
-    'type': 'request',
-    'detail_type': 'new_friend',
+    'time': 1655187651.0,
+    'type': 'message',
+    'detail_type': 'group_temp',
     'sub_type': '',
-    'request_id': 1653473791000000,
-    'user_id': '987654321',
-    'user_name': 'Sclock',
-    'message': ''
+    'group_id': '987654321',
+    'user_name': '',
+    'message_id': '29662',
+    'message': [{'type': 'text',
+                 'data': {'text': '1'}}],
+    'alt_message': '1',
+    'user_id': '1342810270'
 }
 
 # 测试能否实例化
-event = Adapter.json_to_event(test_data, "test_id")
+event = Adapter.json_to_event(test_data,
+                              "test_id")
+
 match event:
     case test_event():
         print("WQFriendAddRequestEvent")
+        print(event.to_me)
+        print(event.is_tome())
         ...
-    case RequestEvent():
-        print("RequestEvent")
+    case MessageEvent():
+        print("MessageEvent")
+        print(event.to_me)
+        print(event.is_tome())
         ...
     case Event():
         print("实例化失败")
